ac59e954870aa08be0a408767cba0a7fdc39b9e4
oo7Fran <53206486+oo7Fran@users.noreply.github.com>
2020-12-21 16:32:25 +0200

    Added Print Counter and Park Nozzle Feature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1519,7 +1519,7 @@
  *    P1  Raise the nozzle always to Z-park height.
  *    P2  Raise the nozzle by Z-park amount, limited to Z_MAX_POS.
  */
-//#define NOZZLE_PARK_FEATURE
+#define NOZZLE_PARK_FEATURE
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
@@ -1632,7 +1632,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 /**
  * Password
